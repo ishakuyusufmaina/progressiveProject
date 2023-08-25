4a7a961dc80ac98a062d366e9dd6c9a6ae3da0a6
ishakuyusufmaina <75148285+ishakuyusufmaina@users.noreply.github.com>
2023-08-25 02:26:04 +0100

    Update msc.js

--- msc/scripts/msc.js
+++ msc/scripts/msc.js
@@ -24,10 +24,10 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const rdb = getDatabase(app);
-//alert("end of firebase setup");
-//alert("rtd object: " + rdb);
-//const remoteBoardRef = ref(rdb, "boards/b1");
-alert(6);
+alert("end of firebase setup");
+alert("rtd object: " + rdb);
+const remoteBoardRef = ref(rdb, "boards/b1");
+alert(remoteBoardRef);
 
 const intface = document.getElementById("interface");
 const board = document.getElementById("board");
